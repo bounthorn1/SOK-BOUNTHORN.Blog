be7a00e3d1edbf25312c42b9ea3e54156c1c2d0c
bounthorn <123523922+bounthorn@users.noreply.github.com>
2026-04-22 08:10:16 +0700

    Fix api to use public db.sdsj

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = import.meta.env.VITE_API_URL || 'https://github.com/bounthorn1/SOK-BOUNTHORN.Blog';
+const API_URL ='https://github.com/bounthorn1/SOK-BOUNTHORN.Blog';
 
 const api = axios.create({
   baseURL: API_URL,
